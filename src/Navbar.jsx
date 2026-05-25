914728d0746ef29078244718a59ab9ce06c44c94
DZIKU77 <sebed1234@gmail.com>
2026-05-25 20:35:15 +0200

    Poczatek, przygotowanie potrzebnych plików początkowych

---
+++ src/Navbar.jsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+    return(
+        <nav className="flex justify-between items-center bg-[rgba(255,255,255,0.15)] p-5">
+           <a href="#" className="uppercase text-black">home</a> 
+        </nav>
+    )
+}
+
+export default Navbar
